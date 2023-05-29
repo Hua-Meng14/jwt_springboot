9cda89dcfd4cb9ef99c7c4aaa1e571fb987e5178
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-05-29 13:50:14 +0700

    add security and jwt

--- src/main/java/com/example/springsecurityjwt/SpringSecurityJwtApplication.java
+++ src/main/java/com/example/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -2,8 +2,10 @@ package com.example.springsecurityjwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.example.spring"})
 public class SpringSecurityJwtApplication {
 
 	public static void main(String[] args) {
